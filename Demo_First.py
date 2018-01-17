876270b7c6459cc30db71f754ab8534e092017ee
ashokramcse <ashokramcse@gmail.com>
2018-01-16 21:10:41 -0800

    Initial Commit

---
+++ Demo_First.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("Hello World!")
+
+firstName = "Ashok" #input("Enter Name: ")
+print ("Customer Name is: %s"%(firstName))
+print (type(firstName))
+
+#Type conversion
+# Number, String, List, Tuple and Dictonary
+# Numbers - int, long(1065L), float and complex(3.14j)
+
+age = int("15") #int(input ("Enter Age: "))
+print ("Age = %d" %(age))
+print (type (age))
+
+#Decimal to Binary
+print (bin(10))
+
+#Binary to Decimal
+print (int("1010", 2))
